d79a28ec095f4b00daed780096638b9aa4a61529
69mrkx <cirillomirko18@gmail.com>
2024-05-13 13:38:53 +0200

    Removing Backend

--- tomatime-back/routes/counterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/counterController.js");
-
-router.get("/:id", controller.getCounters);  
-
-
-module.exports = router;
--- tomatime-back/routes/lcRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/lcControllers.js");
-
-router.get('/:id', controller.getLC);
-router.get('/next/:id', controller.getNextStep);
-
-module.exports = router;
